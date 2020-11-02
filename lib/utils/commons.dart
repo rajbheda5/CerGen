8c1651f604b4753734df262735015fa87eb8fe49
rajbheda5 <rajbheda5@gmail.com>
2020-11-02 21:46:03 +0530

    qr code support + redesign

--- lib/utils/commons.dart
+++ lib/utils/commons.dart
@@ -3,24 +3,28 @@ import 'dart:io';
 
 import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pdf;
+import 'package:pdf/widgets.dart';
 import 'package:printing/printing.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<void> pdfGenerator(name) async {
   var key = utf8.encode('p@ssw0rd');
   var bytes = utf8.encode('$name');
-  
-  var hmacSHA256= new Hmac(sha256, key);
+
+  final certificate = Font.ttf(await rootBundle.load('assets/Certificate.ttf'));
+
+  var hmacSHA256 = new Hmac(sha256, key);
   var digest = hmacSHA256.convert(bytes);
 
   final _pdf = pdf.Document();
   final _assetImage = await pdfImageFromImageProvider(
     pdf: _pdf.document,
     image: AssetImage(
-      'assets/images/account.png',
+      'assets/images/DSC_logo.png',
     ),
   );
   _pdf.addPage(
@@ -32,37 +36,34 @@ Future<void> pdfGenerator(name) async {
           width: double.maxFinite,
           color: PdfColors.deepOrange50,
           child: pdf.Column(
-            mainAxisAlignment: pdf.MainAxisAlignment.center,
+            mainAxisAlignment: pdf.MainAxisAlignment.start,
             crossAxisAlignment: pdf.CrossAxisAlignment.center,
             children: [
               pdf.SizedBox(
-                height: 50,
+                height: 65,
               ),
-              pdf.Container(
-                width: 160,
-                height: 160,
-                decoration: pdf.BoxDecoration(
-                  shape: pdf.BoxShape.circle,
-                  color: PdfColors.deepOrange200,
-                ),
+               pdf.Container(
+                width: 320,
                 child: pdf.Image(_assetImage),
               ),
               pdf.SizedBox(
-                height: 50,
+                height: 60,
               ),
               pdf.Text(
-                'Certificate of Completion',
+                'Certificate of Participation',
                 style: pdf.TextStyle(
-                  fontSize: 22,
-                  color: PdfColors.grey700,
+                  fontSize: 35,
+                  font: certificate,
+                  fontWeight: pdf.FontWeight.bold,
+                  color: PdfColors.grey800,
                 ),
               ),
-              pdf.SizedBox(
-                height: 20,
-              ),
+              pdf.SizedBox(height: 55),
               pdf.Text(
                 'Presented to:',
                 style: pdf.TextStyle(
+                  fontSize: 25,
+                  fontWeight: pdf.FontWeight.normal,
                   color: PdfColors.grey600,
                 ),
               ),
@@ -78,12 +79,26 @@ Future<void> pdfGenerator(name) async {
                   color: PdfColors.grey800,
                 ),
               ),
+              pdf.SizedBox(height: 30),
+              pdf.Text(
+                'for attending DSC Event - Flutter Bootcamp.',
+                style: pdf.TextStyle(
+                  fontSize: 17,
+                  fontWeight: pdf.FontWeight.normal,
+                  color: PdfColors.grey600,
+                ),
+              ),
               pdf.SizedBox(
-                height: 30,
+                height: 75,
               ),
-              pdf.Text('$digest',
-              style: pdf.TextStyle(fontSize: 10),
-              ),
+              // pdf.Text('$digest',
+              // style: pdf.TextStyle(fontSize: 10),
+              // ),
+              pdf.BarcodeWidget(
+                  barcode: pdf.Barcode.qrCode(),
+                  data: digest.toString(),
+                  height: 80,
+                  width: 80)
             ],
           ),
         ),
